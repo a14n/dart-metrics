d0a4e86346f716adb72484cd26ad9384bba56f9d
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-11-26 22:15:02 +0100

    refactoring of console_reporter_tests

--- test/src/console_reporter_tests.dart
+++ test/src/console_reporter_tests.dart
@@ -59,21 +59,28 @@ class MockTimer extends Mock implements Timer {
 }
 
 main() {
+  group('', () {
 
-  test('reports gauge values', () {
-    final registry = new MockMetricRegistry();
-    final clock = new MockClock();
-    clock.when(callsTo('get time')).thenReturn(1363568676000);
+    StringBuffer output;
+    ConsoleReporter reporter;
 
-    final output = new StringBuffer();
-    final reporter = new ConsoleReporter(registry, output: output, clock: clock);
+    setUp(() {
+      final registry = new MockMetricRegistry();
+      final clock = new MockClock();
+      clock.when(callsTo('get time')).thenReturn(1363568676000);
 
-    final gauge = new MockGauge();
-    gauge.when(callsTo('get value')).thenReturn(1);
+      output = new StringBuffer();
+      reporter = new ConsoleReporter(registry, output: output, clock: clock);
 
-    reporter.report({'gauge': gauge}, {}, {}, {}, {});
+    });
 
-    expect(output.toString(), equals('''
+    test('reports gauge values', () {
+      final gauge = new MockGauge();
+      gauge.when(callsTo('get value')).thenReturn(1);
+
+      reporter.report({'gauge': gauge}, {}, {}, {}, {});
+
+      expect(output.toString(), equals('''
 2013-03-18T02:04:36.000 ========================================================
 
 -- Gauges ----------------------------------------------------------------------
@@ -82,22 +89,15 @@ gauge
 
 
 '''));
-  });
+    });
 
-  test('reports counter values', () {
-    final registry = new MockMetricRegistry();
-    final clock = new MockClock();
-    clock.when(callsTo('get time')).thenReturn(1363568676000);
+    test('reports counter values', () {
+      final counter = new MockCounter();
+      counter.when(callsTo('get count')).thenReturn(100);
 
-    final output = new StringBuffer();
-    final reporter = new ConsoleReporter(registry, output: output, clock: clock);
+      reporter.report({}, {'test.counter': counter}, {}, {}, {});
 
-    final counter = new MockCounter();
-    counter.when(callsTo('get count')).thenReturn(100);
-
-    reporter.report({}, {'test.counter': counter}, {}, {}, {});
-
-    expect(output.toString(), equals('''
+      expect(output.toString(), equals('''
 2013-03-18T02:04:36.000 ========================================================
 
 -- Counters --------------------------------------------------------------------
@@ -106,36 +106,29 @@ test.counter
 
 
 '''));
-  });
+    });
 
-  test('reports histogram values', () {
-    final registry = new MockMetricRegistry();
-    final clock = new MockClock();
-    clock.when(callsTo('get time')).thenReturn(1363568676000);
+    test('reports histogram values', () {
+      final histogram = new MockHistogram();
+      histogram.when(callsTo('get count')).thenReturn(1);
 
-    final output = new StringBuffer();
-    final reporter = new ConsoleReporter(registry, output: output, clock: clock);
+      final snapshot = new MockSnapshot();
+      snapshot.when(callsTo('get max')).thenReturn(2);
+      snapshot.when(callsTo('get mean')).thenReturn(3.0);
+      snapshot.when(callsTo('get min')).thenReturn(4);
+      snapshot.when(callsTo('get stdDev')).thenReturn(5.0);
+      snapshot.when(callsTo('get median')).thenReturn(6.0);
+      snapshot.when(callsTo('get75thPercentile')).thenReturn(7.0);
+      snapshot.when(callsTo('get95thPercentile')).thenReturn(8.0);
+      snapshot.when(callsTo('get98thPercentile')).thenReturn(9.0);
+      snapshot.when(callsTo('get99thPercentile')).thenReturn(10.0);
+      snapshot.when(callsTo('get999thPercentile')).thenReturn(11.0);
 
-    final histogram = new MockHistogram();
-    histogram.when(callsTo('get count')).thenReturn(1);
+      histogram.when(callsTo('get snapshot')).thenReturn(snapshot);
 
-    final snapshot = new MockSnapshot();
-    snapshot.when(callsTo('get max')).thenReturn(2);
-    snapshot.when(callsTo('get mean')).thenReturn(3.0);
-    snapshot.when(callsTo('get min')).thenReturn(4);
-    snapshot.when(callsTo('get stdDev')).thenReturn(5.0);
-    snapshot.when(callsTo('get median')).thenReturn(6.0);
-    snapshot.when(callsTo('get75thPercentile')).thenReturn(7.0);
-    snapshot.when(callsTo('get95thPercentile')).thenReturn(8.0);
-    snapshot.when(callsTo('get98thPercentile')).thenReturn(9.0);
-    snapshot.when(callsTo('get99thPercentile')).thenReturn(10.0);
-    snapshot.when(callsTo('get999thPercentile')).thenReturn(11.0);
+      reporter.report({}, {}, {'test.histogram': histogram}, {}, {});
 
-    histogram.when(callsTo('get snapshot')).thenReturn(snapshot);
-
-    reporter.report({}, {}, {'test.histogram': histogram}, {}, {});
-
-    expect(output.toString(), equals('''
+      expect(output.toString(), equals('''
 2013-03-18T02:04:36.000 ========================================================
 
 -- Histograms ------------------------------------------------------------------
@@ -154,26 +147,19 @@ test.histogram
 
 
 '''));
-  });
+    });
 
-  test('reports meter values', () {
-    final registry = new MockMetricRegistry();
-    final clock = new MockClock();
-    clock.when(callsTo('get time')).thenReturn(1363568676000);
+    test('reports meter values', () {
+      final meter = new MockMeter();
+      meter.when(callsTo('get count')).thenReturn(1);
+      meter.when(callsTo('get meanRate')).thenReturn(2.0);
+      meter.when(callsTo('get oneMinuteRate')).thenReturn(3.0);
+      meter.when(callsTo('get fiveMinuteRate')).thenReturn(4.0);
+      meter.when(callsTo('get fifteenMinuteRate')).thenReturn(5.0);
 
-    final output = new StringBuffer();
-    final reporter = new ConsoleReporter(registry, output: output, clock: clock);
+      reporter.report({}, {}, {}, {'test.meter': meter}, {});
 
-    final meter = new MockMeter();
-    meter.when(callsTo('get count')).thenReturn(1);
-    meter.when(callsTo('get meanRate')).thenReturn(2.0);
-    meter.when(callsTo('get oneMinuteRate')).thenReturn(3.0);
-    meter.when(callsTo('get fiveMinuteRate')).thenReturn(4.0);
-    meter.when(callsTo('get fifteenMinuteRate')).thenReturn(5.0);
-
-    reporter.report({}, {}, {}, {'test.meter': meter}, {});
-
-    expect(output.toString(), equals('''
+      expect(output.toString(), equals('''
 2013-03-18T02:04:36.000 ========================================================
 
 -- Meters ----------------------------------------------------------------------
@@ -186,40 +172,33 @@ test.meter
 
 
 '''));
-  });
+    });
 
-  test('reports timer values', () {
-    final registry = new MockMetricRegistry();
-    final clock = new MockClock();
-    clock.when(callsTo('get time')).thenReturn(1363568676000);
+    test('reports timer values', () {
+      final timer = new MockTimer();
+      timer.when(callsTo('get count')).thenReturn(1);
+      timer.when(callsTo('get meanRate')).thenReturn(2.0);
+      timer.when(callsTo('get oneMinuteRate')).thenReturn(3.0);
+      timer.when(callsTo('get fiveMinuteRate')).thenReturn(4.0);
+      timer.when(callsTo('get fifteenMinuteRate')).thenReturn(5.0);
 
-    final output = new StringBuffer();
-    final reporter = new ConsoleReporter(registry, output: output, clock: clock);
+      final snapshot = new MockSnapshot();
+      snapshot.when(callsTo('get max')).thenReturn(const Duration(milliseconds: 100).inMicroseconds);
+      snapshot.when(callsTo('get mean')).thenReturn(const Duration(milliseconds: 200).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get min')).thenReturn(const Duration(milliseconds: 300).inMicroseconds);
+      snapshot.when(callsTo('get stdDev')).thenReturn(const Duration(milliseconds: 400).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get median')).thenReturn(const Duration(milliseconds: 500).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get75thPercentile')).thenReturn(const Duration(milliseconds: 600).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get95thPercentile')).thenReturn(const Duration(milliseconds: 700).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get98thPercentile')).thenReturn(const Duration(milliseconds: 800).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get99thPercentile')).thenReturn(const Duration(milliseconds: 900).inMicroseconds.toDouble());
+      snapshot.when(callsTo('get999thPercentile')).thenReturn(const Duration(milliseconds: 1000).inMicroseconds.toDouble());
 
-    final timer = new MockTimer();
-    timer.when(callsTo('get count')).thenReturn(1);
-    timer.when(callsTo('get meanRate')).thenReturn(2.0);
-    timer.when(callsTo('get oneMinuteRate')).thenReturn(3.0);
-    timer.when(callsTo('get fiveMinuteRate')).thenReturn(4.0);
-    timer.when(callsTo('get fifteenMinuteRate')).thenReturn(5.0);
+      timer.when(callsTo('get snapshot')).thenReturn(snapshot);
 
-    final snapshot = new MockSnapshot();
-    snapshot.when(callsTo('get max')).thenReturn(const Duration(milliseconds: 100).inMicroseconds);
-    snapshot.when(callsTo('get mean')).thenReturn(const Duration(milliseconds: 200).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get min')).thenReturn(const Duration(milliseconds: 300).inMicroseconds);
-    snapshot.when(callsTo('get stdDev')).thenReturn(const Duration(milliseconds: 400).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get median')).thenReturn(const Duration(milliseconds: 500).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get75thPercentile')).thenReturn(const Duration(milliseconds: 600).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get95thPercentile')).thenReturn(const Duration(milliseconds: 700).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get98thPercentile')).thenReturn(const Duration(milliseconds: 800).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get99thPercentile')).thenReturn(const Duration(milliseconds: 900).inMicroseconds.toDouble());
-    snapshot.when(callsTo('get999thPercentile')).thenReturn(const Duration(milliseconds: 1000).inMicroseconds.toDouble());
+      reporter.report({}, {}, {}, {}, {'test.another.timer': timer});
 
-    timer.when(callsTo('get snapshot')).thenReturn(snapshot);
-
-    reporter.report({}, {}, {}, {}, {'test.another.timer': timer});
-
-    expect(output.toString(), equals('''
+      expect(output.toString(), equals('''
 2013-03-18T02:04:36.000 ========================================================
 
 -- Timers ----------------------------------------------------------------------
@@ -242,7 +221,8 @@ test.another.timer
 
 
 '''));
-  });
+    });
 
+  });
 
 }
